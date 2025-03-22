8f6c0228ab1eca7212168742e59ca2dd7940a26a
Gisele Cesar <gisele-cesar@hotmail.com>
2025-03-22 15:19:13 -0300

    Teste infra eks

---
+++ infra/terraform/vpc.tf
@@ -0,0 +1,12 @@
+# Create a VPC
+resource "aws_vpc" "k8s_vpc" {
+  cidr_block = "192.168.0.0/16"
+  tags = {
+    name = "k8s_vpc"
+  }
+}
+
+resource "aws_subnet" "k8s_subnet" {
+  vpc_id     = aws_vpc.k8s_vpc.id
+  cidr_block = "10.0.1.0/24"
+}
